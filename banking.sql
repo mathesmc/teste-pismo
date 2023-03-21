0e8578f56316d93bb19a1ddd61b3a8d5eeebe4c2
matheus.correa <matheus.correa@olist.com>
2023-03-20 23:33:25 -0300

    :pill: fixing database schema for documentation

--- banking.sql
+++ banking.sql
@@ -1,24 +1,28 @@
-CREATE TABLE "accounts" (
+CREATE TABLE IF NOT EXISTS "accounts" (
   "id" bigserial PRIMARY KEY,
-  "document_number" bigint UNIQUE NOT NULL,
-  "created_at" timestamp DEFAULT (now())
+  "document_number" varchar(50) NOT NULL UNIQUE,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
-CREATE TABLE "operation_types" (
+CREATE TABLE IF NOT EXISTS "operation_types" (
   "id" bigserial PRIMARY KEY,
-  "description" varchar NOT NULL,
-  "created_at" timestamp DEFAULT (now())
+  "multiplier" int NOT NULL,
+  "description" varchar(50) NOT NULL UNIQUE,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
-CREATE TABLE "transactions" (
+CREATE TABLE IF NOT EXISTS "transactions" (
   "id" bigserial PRIMARY KEY,
-  "account_id" int NOT NULL,
-  "operation_type_id" int NOT NULL,
-  "amount" bigint NOT NULL,
-  "event_date" timestamp DEFAULT (now())
+  "account_id" bigint NOT NULL,
+  "operation_type_id" bigint NOT NULL,
+  "amount" FLOAT NOT NULL,
+  "event_date" timestamp DEFAULT (now()),
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
 ALTER TABLE "transactions" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
 ALTER TABLE "transactions" ADD FOREIGN KEY ("operation_type_id") REFERENCES "operation_types" ("id");
-
